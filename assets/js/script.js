d84e550279e992c73896368593bfd81089d49d4c
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-09-23 23:43:03 +1000

    feature: created API connection and a working search function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 var $searchFrmEl = $('#search-form');
-var $searchTxtEl = $('#search-text');
+var $searchTxtEl = $('#search-input');
 var $searchBtnEl = $('#search-button');
+
+var $resultCtnEl = $('#result-content');
+
+var $cityResultEl;
 
 var $searchInputTxt;
 
@@ -10,17 +14,72 @@ function getCityName(event) {
     $searchInputTxt = $searchTxtEl.val().trim();
     console.log($searchInputTxt);
 
-    getWeatherAPI($searchInputTxt);
+    if (!$searchInputTxt) {
+        return;
+    }
 
+    getWeatherAPI($searchInputTxt);
 
     $($searchTxtEl).val('');
 }
 
 function getWeatherAPI(city) {
-    APIKey = "467952e5cf21b6ecbc8d9a89b3ec05b9";
-    APIUrl = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var APIKey = "467952e5cf21b6ecbc8d9a89b3ec05b9";
+    var APIUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    // https://api.openweathermap.org/data/2.5/weather?q=Brunei&appid=467952e5cf21b6ecbc8d9a89b3ec05b9
+
+    fetch(APIUrl)
+        .then(function (response) {
+            //console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            displayData(data);
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
 
+function displayData(info) {
+    
+    var $cityName = info.name;
+    var $cityDate = moment(info.dt,"X").format("DD/MM/YYYY");
+    var $weatherIcon;
+
+    for (i=0;i<info.weather.length;i++) {
+        $weatherIcon = 'http://openweathermap.org/img/wn/' + info.weather[i].icon + '.png';
+    }
+
+    console.log($weatherIcon);
+    console.log($cityName);
+    console.log($cityDate);
+
+
+    $cityResultEl = $('<h5>')
+        .addClass('text-uppercase px-2 border border-2')
+        .append($cityName);
+
+    var $cityDate = $('<i>')
+        .append(' [' + $cityDate + '] ');
+    
+    var $weatherIconEl = $('<img>')
+        // .attr('width', '50')
+        // .attr('height', '50')
+        .attr('src', $weatherIcon);
+
+
+    var $cd = $cityResultEl.append($cityDate);
+    $cd.append($weatherIconEl);
+
+    //$resultCtnEl
+    //    .attr('style','background-color: gray;')
+    $resultCtnEl.append($cd);
+    
+
+}
 
 $($searchFrmEl).on('submit', getCityName);
 
